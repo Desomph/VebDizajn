3fe9a3463b8866ad1073f83f76a92747569c0a1f
Desomph <de.nadlacki@gmail.com>
2018-12-23 23:57:18 +0100

    updates...

--- resources/js/data.js
+++ resources/js/data.js
@@ -13,24 +13,18 @@ const news = [
     },
     {
         "id": 3,
-        "image": "https://www.sciencedaily.com/images/2018/04/180425120203_1_540x360.jpg",
-        "title": "Yes, we also have software. Who'd guess.",
-        "body": "Whether you're reinstalling your OS, deciding to switch to designer, or you just wanna remove the rust from your development skills, check out our software section."
-    },
-    {
-        "id": 4,
         "image": "https://i.ytimg.com/vi/OKCL2sF0cBE/maxresdefault.jpg",
         "title": "Blizzard shares dropped substantialy.",
         "body": "Activision Blizzard shares plunged 10 percent in after-hours trading after reporting its third straight quarterly decline in monthly active users. The video game company, which owns the Call of Duty and World of Warcraft franchises, reported 345 million monthly active users, down from 352 million in the previous quarter. "
     },
     {
-        "id": 5,
+        "id": 4,
         "image": "https://s3.dexerto.com/thumbnails/_thumbnailLarge/Is-fallout-76-down-november-21.jpg",
         "title": "New Fallout game backlash..",
         "body": "Bethesda is continuing its apology tour of sorts for Fallout 76's rough state on launch, and this time you don't need to have spent a lot to receive compensation. The developer is promising a free copy of Fallout Classics Collection (1, 2 and Tactics) on PC for everyone who has logged into the full Fallout 76 in 2018, whether they played on a computer or console. You should have access to your freebie in early January, Bethesda said."
     },
     {
-        "id": 6,
+        "id": 5,
         "image": "https://cdn2.unrealengine.com/UnrealEngine%2FNews%2FAnnouncing+the+Epic+Games+Store%2FEpicGamesStore-1400x788-115627d82416826e240d42891ede4afe7975ba19.jpg",
         "title": "New platform launched, Epic Games going forward!",
         "body": "Right now there’s a small cluster of games available, including Hades, a new title from Supergiant Games that is in “early access” for $19.99, and Epic’s own Fortnite and Unreal Tournament, both of which are free. But Epic is saying that’s there’s a lot more to come. In particular, the store will offer a free game every two weeks, starting with Subnautica from December 14-17 and Super Meat Boy from December 28 until January 10."
@@ -40,60 +34,70 @@ const news = [
 const games = [
     {
         "id": 1,
+        "url": "",
         "image": "../media/images/games/igra1.png",
         "name": "World of Warcraft",
         "description": "World of Warcraft (WoW) is a massively multiplayer online role-playing game (MMORPG) released in 2004 by Blizzard Entertainment. It is the fourth released game set in the Warcraft fantasy universe."
     },
     {
         "id": 2,
+        "url": "",
         "image": "../media/images/games/igra7.jpg",
         "name": "Heroes of the Storm",
         "description": "Heroes of the Storm is a multiplayer online battle arena video game developed and published by Blizzard Entertainment for Microsoft Windows and macOS, which released on June 2, 2015."
     },
     {
         "id": 3,
+        "url": "",
         "image": "../media/images/games/igra5.jpg",
         "name": "Diablo III",
         "description": "Diablo III is a dungeon crawler action role-playing video game. It is the third installment in the Diablo franchise and was released on May 15, 2012 for Microsoft Windows and macOS."
     },
     {
         "id": 4,
+        "url": "",
         "image": "../media/images/games/igra4.jpeg",
         "name": "Diablo III - Reaper of Souls",
         "description": "Diablo III: Reaper of Souls is the first expansion pack for the action role-playing video game Diablo III. It was revealed at Gamescom 2013."
     },
     {
         "id": 5,
+        "url": "",
         "image": "../media/images/games/igra3.jpg",
         "name": "Starcraft II",
         "description": "StarCraft II is a military science fiction real-time strategy video game developed and published by Blizzard Entertainment. It was released worldwide in July 2010 for Microsoft Windows and Mac OS X."
     },
     {
         "id": 6,
+        "url": "",
         "image": "../media/images/games/igra2.jpg",
         "name": "Overwatch",
         "description": "Overwatch is a team-based multiplayer first-person shooter video game developed and published by Blizzard Entertainment, which released on May 24, 2016 for PlayStation 4, Xbox One, and Windows. Described as a \"hero shooter\"."
     },
     {
         "id": 7,
+        "url": "",
         "image": "../media/images/games/igra10.jpg",
         "name": "Fortnite",
         "description": "Fortnite is an online video game first released in 2017 and developed by Epic Games. It is available as separate software packages having different game modes that otherwise share the same general gameplay and game engine."
     },
     {
         "id": 8,
+        "url": "",
         "image": "../media/images/games/igra8.jpg",
         "name": "Hearthstone",
         "description": "Hearthstone, originally Hearthstone: Heroes of Warcraft, is a free-to-play online collectible card video game."
     },
     {
         "id": 9,
+        "url": "",
         "image": "../media/images/games/igra6.jpg",
         "name": "Starcraft - Remastered",
         "description": "StarCraft: Remastered is a remastered edition of the 1998 real-time strategy video game StarCraft and its expansion Brood War, which was released on August 14, 2017."
     },
     {
         "id": 10,
+        "url": "",
         "image": "../media/images/games/igra9.jpg",
         "name": "Dota 2",
         "description": "Dota 2 is a free-to-play multiplayer online battle arena (MOBA) video game developed and published by Valve Corporation. The game is the stand-alone sequel to Defense of the Ancients (DotA)."
--- resources/js/news.js
+++ resources/js/news.js
@@ -2,17 +2,19 @@
     window.addEventListener("load", () => {
         console.log("We're ready bois!");
 
-        news.forEach(newz => {
-            $("#news-container").append(`            
-            <div class="news-box">
-                <img class="news-image" src="${newz.image}">
-                </img>
-                <h1>${newz.title}</h1>
-                <hr class="news-box-hr" />
-                <h3>
-                    <p>${newz.body}</p>
-                </h3>
-            </div>`);
-        });
+        if (news != null && news.length > 0) {
+            news.forEach(newz => {
+                $("#news-container").append(`            
+                <div class="news-box">
+                    <img class="news-image" src="${newz.image}">
+                    </img>
+                    <h1>${newz.title}</h1>
+                    <hr class="news-box-hr" />
+                    <h3>
+                        <p>${newz.body}</p>
+                    </h3>
+                </div>`);
+            });
+        }
     });
 })();
