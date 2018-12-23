d8bfc210f5782510d7a5298c2413c8a7b793dd42
Desomph <de.nadlacki@gmail.com>
2018-12-23 17:01:35 +0100

    update

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,17 +1,5 @@
-var elements;
-const log = () => {
-    Array.from(document.getElementsByClassName("game-box")).forEach(function (item) {
-        item.setAttribute("style", `background-color: rgb(${generateRandomNumber(0, 255)}, ${generateRandomNumber(0, 255)}, ${generateRandomNumber(0, 255)});`);
-        console.log("didit");
+(() => {
+    window.addEventListener("load", () => {
+        console.log("We're ready bois!");
     });
-}
-
-function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-const ready = (event) => {
-    var interval = setInterval(log, 2000);
-}
-
-window.addEventListener("load", ready);
+})();
